651dd786c93a49ccc423f924794598751b2d74ea
inn372awesome@gmail.com <inn372awesome@gmail.com@566dcdf4-d801-b303-7cc6-568b5576cba2>
2011-08-27 14:24:18 +0000

    New version by committing LEE's files ---by Brian

--- branches/Brian/src/mediavault/Upload.java
+++ branches/Brian/src/mediavault/Upload.java
@@ -24,35 +24,33 @@ import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class Upload extends HttpServlet {
-	private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory
+			.getBlobstoreService();
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-        throws ServletException, IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
 
-    	UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
-        
-        Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
-        BlobKey blobKey = blobs.get("myFile");
-        
-        Key fileKey = KeyFactory.createKey("fileKey", "fileKey");
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        BlobInfoFactory blobinfofactory = new BlobInfoFactory();
-        BlobInfo blobinfo = blobinfofactory.loadBlobInfo(blobKey);
-        Entity fileinfo = new Entity("fileinfo", fileKey);
-        fileinfo.setProperty("blobkey", blobKey);
-        fileinfo.setProperty("date", blobinfo.getCreation());
-        fileinfo.setProperty("filename", blobinfo.getFilename());
-        fileinfo.setProperty("owner", user);
-        fileinfo.setProperty("size", blobinfo.getSize());
-        fileinfo.setProperty("contenttype", blobinfo.getContentType());
-        
-        datastore.put(fileinfo);
-        
-        if (blobKey == null) {
-            res.sendRedirect("/");
-        } else {
-            res.sendRedirect("/file_list.jsp");
-        }
-    }
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+
+		Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+		BlobKey blobKey = blobs.get("myFile");
+
+		Key fileKey = KeyFactory.createKey("fileKey", "fileKey");
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		BlobInfoFactory blobinfofactory = new BlobInfoFactory();
+		BlobInfo blobinfo = blobinfofactory.loadBlobInfo(blobKey);
+		Entity fileinfo = new Entity("fileinfo", fileKey);
+		fileinfo.setProperty("blobkey", blobKey);
+		fileinfo.setProperty("date", blobinfo.getCreation());
+		fileinfo.setProperty("filename", blobinfo.getFilename());
+		fileinfo.setProperty("owner", user);
+		fileinfo.setProperty("size", blobinfo.getSize());
+		fileinfo.setProperty("contenttype", blobinfo.getContentType());
+
+		datastore.put(fileinfo);
+
+		res.sendRedirect("/");
+	}
 }
